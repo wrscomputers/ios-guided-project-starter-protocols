cf98dd3a6c73648dec39e2a4c91f76bcbd7b8ca1
Ben Gohlke <jcgohlke@me.com>
2019-09-10 19:59:16 -0400

    Added starter playground.

---
+++ Protocols.playground/Pages/Game.xcplaygroundpage/Contents.swift
@@ -0,0 +1,52 @@
+import Foundation
+//: We're building a dice game called _Knock Out!_. It is played using the following rules:
+//: 1. Each player chooses a “knock out number” – either 6, 7, 8, or 9. More than one player can choose the same number.
+//: 2. Players take turns throwing both dice, once each turn. Add the number of both dice to the player's running score.
+//: 3. If a player rolls their own knock out number, they are knocked out of the game.
+//: 4. Play ends when either all players have been knocked out, or if a single player scores 100 points or higher.
+//:
+//: Let's reuse some of the work we defined from the previous page.
+
+protocol GeneratesRandomNumbers {
+    func random() -> Int
+}
+
+class OneThroughTen: GeneratesRandomNumbers {
+    func random() -> Int {
+        return Int.random(in: 1...10)
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: GeneratesRandomNumbers
+    
+    init(sides: Int, generator: GeneratesRandomNumbers) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() % sides) + 1
+    }
+}
+
+//: Now, let's define a couple protocols for managing a dice-based game.
+
+
+
+//: Lastly, we'll create a custom class for tracking a player in our dice game.
+
+
+
+//: With all that configured, let's build our dice game class called _Knock Out!_
+
+
+
+//: The following class is used to track the status of the above game, and will conform to the `DiceGameDelegate` protocol.
+
+
+
+//: Finally, we need to test out our game. Let's create a game instance, add a tracker, and instruct the game to play.
+
+
---
+++ Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+//: # Protocols
+//: Protocols are, as per Apple's definition in the _Swift Programming Language_ book:
+//:
+//: "... a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol."
+//:
+//: The below example shows a protocol that requires conforming types have a particular property defined.
+
+
+
+//: Protocols can also require that conforming types implement certain methods.
+
+
+
+//: ## Protocols as Types
+
+
